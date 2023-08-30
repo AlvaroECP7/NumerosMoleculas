609fcbd915df39e6220ea0b8bcf8a6d33b922ed1
AlvaroECP7 <143287583+AlvaroECP7@users.noreply.github.com>
2023-08-30 10:24:33 -0500

    Add files via upload

--- Clase.py
+++ Clase.py
@@ -1,10 +1,17 @@
+# Crea una matriz de 3x3 e imprime la suma de los elementos en su diagonal (traza).
+matriz = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]]
 
-import sys
-import keyword
+suma = sum(matriz[i][i] for i in range(3))
+suma2 = sum(matriz[i][2 - i] for i in range(3))
 
-print("Versión de Python:", sys.version)
 
-palabras_reservadas = keyword.kwlist
-print("\nPalabras reservadas de Python:")
-for palabra in palabras_reservadas:
-    print("- " + palabra)
+print("Matriz:")
+for numero in matriz:
+    print(numero)
+
+
+print("\nSuma de la diagonal (135`):", suma)
+print("\nSuma de la diagonal (45`):", suma2)
