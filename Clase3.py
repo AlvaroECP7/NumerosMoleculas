0832d460b76c93b09c9248bf4413e0611f8cf861
AlvaroECP7 <143287583+AlvaroECP7@users.noreply.github.com>
2023-08-30 10:34:10 -0500

    Clase3.py

---
+++ Clase3.py
@@ -0,0 +1,15 @@
+# Guarda como variable la masa atómica de los elementos C, H y O, e imprime la masa molecular del etanol.
+masa_atomica_C = 12.01
+masa_atomica_H = 1.01
+masa_atomica_O = 16.00
+
+
+numero_atomos_C = 2
+numero_atomos_H = 5
+numero_atomos_O = 1
+
+
+masa_molecular_etanol = (numero_atomos_C * masa_atomica_C) + (numero_atomos_H * masa_atomica_H) + (numero_atomos_O * masa_atomica_O)
+
+
+print("Masa molecular del etanol (C2H5OH):", masa_molecular_etanol)
